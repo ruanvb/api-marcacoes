1b62b075b5701842ab29207be6210f5ec8e18c40
ruanvb <ruanvictor2001@outlook.com>
2022-11-13 11:20:01 -0300

    feat: adicionado tratamento para demais tags

--- api-cadastra-marcacao.py
+++ api-cadastra-marcacao.py
@@ -17,28 +17,34 @@ def inlcuir_marcacao():
     if nova_marcacao["includedAt"] == 'null':
         retorno = [{'message': 'Erro: a data da marcação ''includedAt'' não foi informada.'}]
         return jsonify(retorno), 500
+    elif nova_marcacao["employeeId"] == 'null':
+        retorno = [{'message': 'Erro: a empresa origem ''employeeId'' não foi informada.'}]
+        return jsonify(retorno), 500
+    elif nova_marcacao["employerId"] == 'null':
+        retorno = [{'message': 'Erro: o colaborador ''employerId'' não foi informada.'}]
+        return jsonify(retorno), 500
+    else:
+        parametros = pika.URLParameters(rabbitmq_server["url_connection"])
+        connection = pika.BlockingConnection(parametros)
+        channel = connection.channel()
+        channel.queue_declare(queue=rabbitmq_server["queue"], durable=True)
+        channel.confirm_delivery()
+        channel.basic_publish(exchange='',
+                            routing_key='marcacao-ponto',
+                            body=str(nova_marcacao),
+                            properties=pika.BasicProperties(delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE))
 
-    parametros = pika.URLParameters(rabbitmq_server["url_connection"])
-    connection = pika.BlockingConnection(parametros)
-    channel = connection.channel()
-    channel.queue_declare(queue=rabbitmq_server["queue"], durable=True)
-    channel.confirm_delivery()
-    channel.basic_publish(exchange='',
-                        routing_key='marcacao-ponto',
-                        body=str(nova_marcacao),
-                        properties=pika.BasicProperties(delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE))
+        connection.close()
 
-    connection.close()
+        retorno = [
+            {
+                'includedAt': nova_marcacao["includedAt"],
+                'employeeId': nova_marcacao["employeeId"],
+                'employerId': nova_marcacao["employerId"]
+            }
+        ]
 
-    retorno = [
-        {
-            'includedAt': nova_marcacao["includedAt"],
-            'employeeId': nova_marcacao["employeeId"],
-            'employerId': nova_marcacao["employerId"]
-        }
-    ]
-
-    return jsonify(retorno)
+        return jsonify(retorno)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')

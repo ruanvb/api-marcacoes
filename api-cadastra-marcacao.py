799457fc337ca1c0e76fc303a425f2f46a520efa
ruanvb <ruanvictor2001@outlook.com>
2022-11-12 21:14:55 -0300

    feat: adicionada confirmação de recebimento

--- api-cadastra-marcacao.py
+++ api-cadastra-marcacao.py
@@ -1,8 +1,4 @@
-# 1. Objetivo - Criar uma API que possibilite cadastrar marcações para envio ao RabbitMQ
-# 2. URL Base - localhost
-# 3. Endpoints -
-    # - localhost/marcacao
-# 4. Recursos - registro da marcação do ponto
+#API Responsável por receber e enviar as marcações ao RabbitMQ
 
 from flask import Flask, jsonify, request, render_template
 import pika
@@ -17,14 +13,15 @@ def pagina_inicial():
 def inlcuir_marcacao():
     nova_marcacao = request.get_json()
     
-    parameters = pika.URLParameters('amqps://qsvvrqtm:XCvydaLcdvhxgwOUKjXAfviC3G30nK8f@woodpecker.rmq.cloudamqp.com/qsvvrqtm')
-    connection = pika.BlockingConnection(parameters)
+    parametros = pika.URLParameters('amqps://qsvvrqtm:XCvydaLcdvhxgwOUKjXAfviC3G30nK8f@woodpecker.rmq.cloudamqp.com/qsvvrqtm')
+    connection = pika.BlockingConnection(parametros)
     channel = connection.channel()
     channel.queue_declare(queue='marcacao-ponto', durable=True)
-
+    channel.confirm_delivery()
     channel.basic_publish(exchange='',
                         routing_key='marcacao-ponto',
-                        body=str(nova_marcacao))
+                        body=str(nova_marcacao),
+                        properties=pika.BasicProperties(delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE))
 
     connection.close()
 
